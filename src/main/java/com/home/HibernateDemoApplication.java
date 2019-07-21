b2e973b7e1b5cce6906015eb1088cb24de536983
Ranjan Ghosh <ranjanghosh9231@gmail.com>
2019-07-21 23:20:06 +0530

    Initial Commit

---
+++ src/main/java/com/home/HibernateDemoApplication.java
@@ -0,0 +1,15 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class HibernateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateDemoApplication.class, args);
+	}
+
+}
